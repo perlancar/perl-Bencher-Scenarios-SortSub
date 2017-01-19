6a87e77c608d8c49d5fcacf7100add98237087eb
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2017-01-19 09:54:50 +0700

    Add participants: Sort::Naturally::XS::nsort, Sort::Naturally::XS::ncmp, Sort::Naturally::ncmp

--- lib/Bencher/Scenario/SortSub/naturally.pm
+++ lib/Bencher/Scenario/SortSub/naturally.pm
@@ -24,6 +24,22 @@ our $scenario = {
             fcall_template => 'Sort::Naturally::nsort(@{<data>})',
             result_is_list => 1,
         },
+        {
+            name => 'Sort::Naturally::ncmp',
+            module => 'Sort::Naturally',
+            code_template => 'sort {Sort::Naturally::ncmp($a, $b)} @{<data>}',
+            result_is_list => 1,
+        },
+        {
+            fcall_template => 'Sort::Naturally::XS::nsort(@{<data>})',
+            result_is_list => 1,
+        },
+        {
+            name => 'Sort::Naturally::XS::ncmp',
+            module => 'Sort::Naturally::XS',
+            code_template => 'sort {Sort::Naturally::XS::ncmp($a, $b)} @{<data>}',
+            result_is_list => 1,
+        },
     ],
     datasets => [
         { name=>'10items' , args=>{data=>[shuffle map { "track$_.mp3" } 1..10 ]} },
